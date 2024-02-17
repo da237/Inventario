aa28176f63cebd62a5f29a347ecaec234cd722bd
Daniel <daniel951931@outlook.es>
2024-02-16 23:17:25 -0500

    Se realiza la creacion de carpetas del proyecto y se hace la vinculacion del framework bulma

---
+++ vistas/home.php
@@ -0,0 +1,11 @@
+<div class="container is-fluid">
+    <h1 class="tile">Home</h1>
+    <h2 class="subtitle">!Biemvenido usuario!</h2>
+
+</div>
+
+<?php
+
+
+
+?>
---
+++ vistas/login.php
@@ -0,0 +1,23 @@
+<div class="main-container">
+    <form class="box login" action="" method="post" autocomplete="off">
+        <h5 class="title is-5 has-text-centered is-uppercase">Sistema de inventario</h5>
+        <div class="field">
+            <label class="label">Usuario</label>
+            <div class="control">
+                <input class="input" type="text" name="login_usuario"
+                pattern="[a-zA]">
+            </div>
+        </div>
+
+    </form>
+
+
+</div>
+
+
+
+
+<?php
+
+
+?>
